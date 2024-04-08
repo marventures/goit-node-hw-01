3f91c1265b4be1647ce6bccad0affd936f685279
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2024-04-08 15:36:38 +0800

    Add code

---
+++ index.js
@@ -0,0 +1,47 @@
+import { program } from "commander";
+import {
+  getContactById,
+  listContacts,
+  removeContact,
+  addContact,
+} from "./contacts.js";
+
+program
+  .option("-a, --action <type>")
+  .option("-i, --id <type>")
+  .option("-n, --name <type>")
+  .option("-e, --email <type>")
+  .option("-p, --phone <type>");
+
+program.parse();
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.log(contacts);
+      break;
+
+    case "get":
+      const contactsById = await getContactById(id);
+      console.log(contactsById);
+      break;
+
+    case "add":
+      const addNewContact = await addContact({ name, email, phone });
+      console.log(addNewContact);
+      break;
+
+    case "remove":
+      const deletedContact = await removeContact(id);
+      console.log(deletedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+invokeAction(argv);
